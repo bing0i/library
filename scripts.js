1dd5480768ae87eddee68ccf9bdb3908e064106b
Courgette <courgett3@gmail.com>
2020-07-04 13:41:22 -0500

    Add new book to table

--- scripts.js
+++ scripts.js
@@ -12,16 +12,40 @@ Book.prototype.info = function() {
 let myLibrary = []
 
 function addBookToLibrary() {
-   const btnAdd = document.querySelector('#btnAdd');
+    const btnAdd = document.querySelector('#btnAdd');
     btnAdd.addEventListener('click', () =>  {
         let newBook = new Book(document.querySelector('#title').value, 
                                 document.querySelector('#author').value,
                                 document.querySelector('#pages').value, 
                                 document.querySelector('#read').value)
         myLibrary.push(newBook)
+        render(newBook, myLibrary.length - 1)
         console.log(myLibrary)
-        myLibrary.forEach(element => console.log(element))
     })
+}
+
+function render(newBook, index) {
+    let tbodyBook = document.querySelector('#tbodyBook')
+    let tr = document.createElement('tr')
+    let td = document.createElement('td')
+    for(let prop in newBook) {
+        if (newBook.hasOwnProperty(prop)) {
+            td.textContent = newBook[prop]
+            tr.appendChild(td)
+            td = document.createElement('td')
+        }
+    }
+
+    let btnRemove = document.createElement('button')
+    btnRemove.setAttribute('id', index.toString())
+    btnRemove.textContent = "Remove"
+    btnRemove.addEventListener('click', () => {
+        myLibrary.splice(parseInt(btnRemove.id), 1)
+        console.log(myLibrary)
+    })
+
+    tr.appendChild(btnRemove)
+    tbodyBook.appendChild(tr)
 }
 
 addBookToLibrary()
