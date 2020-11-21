fb3015e6e8890315bcc775bf720948e26639d8f7
Courgette <courgett3@gmail.com>
2020-11-20 23:31:33 -0500

    Refractor Book to use class (instead of plain constructor)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-function Book(title, author, pages, read) {
+class Book {
+  constructor(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+  }
+
+  info() {
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "read" : "not read yet")
+  }
 }
 
-Book.prototype.info = function() {
-    return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.read ? "read" : "not read yet")
-}
 
 let myLibrary = []
 
